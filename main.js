664af26c2239f3cc2c94f7739df424258c39f6a0
hussein chakroun <57619493+hussein-chakroun@users.noreply.github.com>
2023-04-06 09:45:36 +0300

    hi

--- main.js
+++ main.js
@@ -1,80 +1,79 @@
 function $(s) {
-  return document.querySelector(s);
+    return document.querySelector(s);
 }
 document.querySelectorAll(".project").forEach((proj) => {
-  proj.addEventListener("mousemove", (e) => {
-    proj.setAttribute(
-      "style",
-      `--x: ${
+    proj.addEventListener("mousemove", (e) => {
+        proj.setAttribute(
+            "style",
+            `--x: ${
         (-2 * (e.layerY - proj.clientHeight / 2)) / proj.clientHeight
       }; --y: ${(2 * (e.layerX - proj.clientWidth / 2)) / proj.clientWidth}`
-    );
-  });
-  proj.addEventListener("mouseleave", () => {
-    proj.setAttribute("style", "--x: 0; --y: 0");
-  });
-  proj.addEventListener("blur", () => {
-    proj.setAttribute("style", "--x: 0; --y: 0");
-  });
+        );
+    });
+    proj.addEventListener("mouseleave", () => {
+        proj.setAttribute("style", "--x: 0; --y: 0");
+    });
+    proj.addEventListener("blur", () => {
+        proj.setAttribute("style", "--x: 0; --y: 0");
+    });
 });
 
 $("#contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  $("#name").value = "";
-  $("#email").value = "";
-  $("#message").value = "";
+    e.preventDefault();
+    $("#name").value = "";
+    $("#email").value = "";
+    $("#message").value = "";
 });
 
 document.querySelectorAll("section").forEach((s) => {
-  s.addEventListener("click", () => {
-    $("#nav-wrapper").classList.remove("open-burger");
-  });
+    s.addEventListener("click", () => {
+        $("#nav-wrapper").classList.remove("open-burger");
+    });
 });
 
 $("footer").addEventListener("click", () => {
-  $("#nav-wrapper").classList.remove("open-burger");
+    $("#nav-wrapper").classList.remove("open-burger");
 });
 
 $("#nav-toggle-btn").addEventListener("click", () => {
-  $("#nav-wrapper").classList.toggle("open-burger");
+    $("#nav-wrapper").classList.toggle("open-burger");
 });
 
 let angle = 0;
 
 function projSpin(sign) {
-  spinner = document.querySelector("figure");
-  if (!sign) {
-    angle = angle + 60;
-  } else {
-    angle = angle - 60;
-  }
-  spinner.setAttribute(
-    "style",
-    "-webkit-transform: rotateY(" +
-      angle +
-      "deg); -moz-transform: rotateY(" +
-      angle +
-      "deg); transform: rotateY(" +
-      angle +
-      "deg);"
-  );
+    spinner = document.querySelector("figure");
+    if (!sign) {
+        angle = angle + 60;
+    } else {
+        angle = angle - 60;
+    }
+    spinner.setAttribute(
+        "style",
+        "-webkit-transform: rotateY(" +
+        angle +
+        "deg); -moz-transform: rotateY(" +
+        angle +
+        "deg); transform: rotateY(" +
+        angle +
+        "deg);"
+    );
 }
 
 
-const blob = document.getElementById("blob") 
+const blob = document.getElementById("blob")
 
 document.querySelectorAll("section").forEach(s => {
 
-	s.addEventListener("mousemove", (e) => {
-		//if(e.pageY + 200 < document.body.clientHeight) 
-		blob.style.top = `${e.pageY - 100}px`;
-		blob.style.left = `${e.pageX - 100}px`;
-		blob.setAttribute("data-y", `${e.screenY}`)
-	})
+    s.addEventListener("mousemove", (e) => {
+        //if(e.pageY + 200 < document.body.clientHeight) 
+        blob.style.top = `${e.pageY - 100}px`;
+        blob.style.left = `${e.pageX - 100}px`;
+        blob.setAttribute("data-y", `${e.screenY}`)
+    })
 
 })
 
 window.addEventListener("scroll", () => {
-	blob.style.top = `${window.scrollY + parseInt(blob.getAttribute("data-y")) - 200}px`
+    blob.style.top = `${window.scrollY + parseInt(blob.getAttribute("data-y")) - 200}px`
 });
-
